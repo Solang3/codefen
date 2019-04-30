6e83cf1cc80011bbba1bd381763cb1176dcce337
solang3 <solangegf@gmail.com>
2019-04-30 15:42:40 -0300

    Updates

---
+++ precache-manifest.9cda289a12ef213b03858cb2cd3fe798.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "399201153ea6621386530069118c5f76",
+    "url": "/codefen/index.html"
+  },
+  {
+    "revision": "19d5ad8672413713eef2",
+    "url": "/codefen/static/css/2.a414b599.chunk.css"
+  },
+  {
+    "revision": "8a78d3c3389cbeff4951",
+    "url": "/codefen/static/css/main.9fe5c031.chunk.css"
+  },
+  {
+    "revision": "19d5ad8672413713eef2",
+    "url": "/codefen/static/js/2.480dde9c.chunk.js"
+  },
+  {
+    "revision": "8a78d3c3389cbeff4951",
+    "url": "/codefen/static/js/main.58f14bf4.chunk.js"
+  },
+  {
+    "revision": "e71c4ad3295f6cf1cced",
+    "url": "/codefen/static/js/runtime~main.dc515d60.js"
+  }
+]);
