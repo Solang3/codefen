c5bb410b537690f76fada67cb4aff35bc3553f46
solang3 <solangegf@gmail.com>
2019-05-02 11:18:36 -0300

    Removed route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,49 +1,17 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { history } from '../../helpers';
-import { alertActions } from '../../actions'
 import Authenticate from '../Authenticate/Authenticate';
-import PrivateRoute from '../PrivateRoute/PrivateRoute';
-import DashBoard from '../DashBoard/DashBoard';
 import { Layout } from 'antd';
 
 import './App.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    const { dispatch } = this.props;
-    history.listen((location, action) => {
-      // clear alert on location change
-      dispatch(alertActions.clear());
-    });
-  }
-
   render() {
-    const { alert } = this.props;
     return (
       <Layout className="codefen-app">
-        {alert.message &&
-          <div className={`alert ${alert.type}`}>{alert.message}</div>
-        }
-        <Router history={history}>
-            <PrivateRoute exact path="/dashboard" component={DashBoard} />
-            <Route path="/" component={Authenticate} />
-        </Router>
+        <Authenticate />
       </Layout>
     )
   }
 }
 
-
-function mapStateToProps(state) {
-  const { alert } = state;
-  return {
-      alert
-  };
-}
-
-const connectedApp = connect(mapStateToProps)(App);
-export { connectedApp as App }; 
+export default App; 
